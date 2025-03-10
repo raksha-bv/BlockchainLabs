691f55e801d9f27acc6d37b50dd53a439c6dd46c
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2025-03-10 22:42:46 +0530

    chore : added Gemini Logo

--- Frontend/components/HeroSection.tsx
+++ Frontend/components/HeroSection.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Gemini from "./ui/Gemini";
 
 const HeroSection = () => {
   return (
@@ -139,7 +140,7 @@ const HeroSection = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  Project Templates
+                  Blockchain Lessons
                 </li>
               </ul>
             </motion.div>
@@ -185,9 +186,7 @@ const HeroSection = () => {
               animate={{ opacity: 1 }}
               transition={{ delay: 1, duration: 0.7 }}
             >
-              <span className="text-xs text-gray-400">
-                Powered By
-              </span>
+              <span className="text-xs text-gray-400">Powered By</span>
               <div className="flex space-x-4 opacity-60">
                 <svg
                   className="h-5 text-gray-400"
@@ -196,13 +195,7 @@ const HeroSection = () => {
                 >
                   <path d="M12 0C5.4 0 0 5.4 0 12c0 5.3 3.4 9.8 8.2 11.4.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.5-1.3-1.3-1.7-1.3-1.7-1.1-.7.1-.7.1-.7 1.2.1 1.8 1.2 1.8 1.2 1.1 1.8 2.8 1.3 3.5 1 .1-.8.4-1.3.8-1.6-2.8-.3-5.6-1.4-5.6-6.1 0-1.3.5-2.4 1.2-3.3-.1-.3-.5-1.5.1-3.2 0 0 1-.3 3.3 1.2.9-.3 1.8-.4 2.8-.4s1.9.1 2.8.4c2.2-1.5 3.2-1.2 3.2-1.2.6 1.7.2 2.9.1 3.2.8.9 1.2 2 1.2 3.3 0 4.7-2.8 5.8-5.6 6.1.4.4.8 1.1.8 2.2v3.3c0 .3.2.7.8.6 4.8-1.6 8.2-6.1 8.2-11.4C24 5.4 18.6 0 12 0z" />
                 </svg>
-                <svg
-                  className="h-5 text-gray-400"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.223 0h.002z" />
-                </svg>
+                <Gemini />
                 <svg
                   className="h-5 text-gray-400"
                   viewBox="0 0 24 24"
